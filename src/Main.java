4844eea0cca0a098fa35aa8f9d97f5f10cb6a747
Christina Kungurtseva <kungurts@gmail.com>
2022-09-29 12:33:37 +0300

    добавлен класс Main, файл README.md

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int[] salesOfYear = {12, 4, 38, 17, 39, 4};
+        SalesManager sales = new SalesManager(salesOfYear);
+
+        System.out.println(sales.max());
+
+    }
+}
